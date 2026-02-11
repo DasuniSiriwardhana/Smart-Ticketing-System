1f63beeee2d3d4811aadaf3eea6a40173492ab85
Dasuni Siriwardhana <siriwardhanadasuni183@gmail.com>
2026-02-11 21:50:13 +0530

    Added TICKET model, scaffolded controller and applied migration

---
+++ SmartTicketingSystem/SmartTicketingSystem/Models/INQUIRY.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartTicketingSystem.Models
+{
+    public class INQUIRY
+    {
+        [Key]
+        public int InquiryID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string category { get; set; }
+        public string message { get; set; }
+        public string status { get; set; }
+
+      
+       
+    }
+}
